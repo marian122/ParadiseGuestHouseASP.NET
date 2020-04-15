1292c0383306881925614e5e95ab6a8b30feafd3
marian122 <maleksiev12@gmail.com>
2020-04-15 13:59:06 +0300

    Implemented new tests in RoomServiceTests and Implemented test for ConferenceHallService

---
+++ ParadiseGuestHouse.Services.Tests/ConferenceHallServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParadiseGuestHouse.Data;
+using ParadiseGuestHouse.Data.Models;
+using ParadiseGuestHouse.Data.Models.Enums;
+using ParadiseGuestHouse.Data.Repositories;
+using ParadiseGuestHouse.Services.Data;
+using ParadiseGuestHouse.Services.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParadiseGuestHouse.Services.Tests
+{
+    public class ConferenceHallServiceTests
+    {
+        private ApplicationDbContext dbContext;
+        private ConferenceHallService conferenceHallService;
+
+        public class MyTestConferenceHall : IMapFrom<ConferenceHall>
+        {
+            public string Id { get; set; }
+        }
+
+        [Fact]
+        public async Task Check_ConferenceHall_Seeder()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                          .Options;
+
+            this.dbContext = new ApplicationDbContext(options);
+
+            var conferenceHallRepository = new EfDeletableEntityRepository<ConferenceHall>(this.dbContext);
+            var conferenceHallReservationRepository = new EfDeletableEntityRepository<ConferenceHallReservation>(this.dbContext);
+
+            conferenceHallRepository.AddAsync(new ConferenceHall
+            {
+                Id = "conferenceid",
+                CreatedOn = DateTime.UtcNow,
+                IsDeleted = false,
+                EventType = (ConfHallEventType)Enum.Parse(typeof(ConfHallEventType), "Conference"),
+                Description = "Conference",
+                CurrentCapacity = 100,
+                MaxCapacity = 100,
+                Price = 50,
+            }).GetAwaiter().GetResult();
+
+            conferenceHallRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            this.conferenceHallService = new ConferenceHallService(conferenceHallReservationRepository, conferenceHallRepository);
+
+            AutoMapperConfig.RegisterMappings(typeof(MyTestConferenceHall).Assembly);
+
+            var actualResult = conferenceHallRepository.All().ToList();
+
+            int expectedResult = 1;
+            Assert.Equal(actualResult.Count(), expectedResult);
+        }
+    }
+}
--- ParadiseGuestHouse.Services.Tests/RoomServiceTests.cs
+++ ParadiseGuestHouse.Services.Tests/RoomServiceTests.cs
@@ -6,6 +6,7 @@ using ParadiseGuestHouse.Data.Models;
 using ParadiseGuestHouse.Data.Models.Enums;
 using ParadiseGuestHouse.Data.Repositories;
 using ParadiseGuestHouse.Services.Data;
+using ParadiseGuestHouse.Services.Mapping;
 using ParadiseGuestHouse.Tests.Common;
 using ParadiseGuestHouse.Web.InputModels.Room;
 using ParadiseGuestHouse.Web.ViewModels.RoomViewModels;
@@ -21,6 +22,20 @@ namespace ParadiseGuestHouse.Services.Tests
     {
         private ApplicationDbContext dbContext;
         private RoomsService roomService;
+
+        public class MyTestRoom : IMapFrom<Room>
+        {
+            public string Id { get; set; }
+
+            public string RoomType { get; set; }
+        }
+
+        public class MyTestReservation : IMapFrom<RoomReservation>
+        {
+            public string RoomId { get; set; }
+
+            public string UserId { get; set; }
+        }
 
         [Fact]
         public async Task ManualCreateRoom_WithValidData_ShouldReturnCorrectRoom()
@@ -296,5 +311,242 @@ namespace ParadiseGuestHouse.Services.Tests
 
             Assert.True(actual);
         }
+
+        [Fact]
+        public async Task Get_All_Rooms_Should_Return_All_Rooms()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                          .Options;
+
+            this.dbContext = new ApplicationDbContext(options);
+
+            var roomRepository = new EfDeletableEntityRepository<Room>(this.dbContext);
+            var roomReservationRepository = new EfDeletableEntityRepository<RoomReservation>(this.dbContext);
+            var pictureRepository = new EfDeletableEntityRepository<Picture>(this.dbContext);
+            var pictureService = new PictureService(pictureRepository);
+            var moqCloudinaryService = new Mock<ICloudinaryService>();
+
+            roomRepository.AddAsync(new Room
+            {
+                RoomType = (RoomType)Enum.Parse(typeof(RoomType), "SingleRoom"),
+                Price = 10,
+                Pictures = null,
+                HasAirConditioner = true,
+                HasBathroom = true,
+                HasHeater = false,
+                HasMountainView = false,
+                HasPhone = false,
+                HasRoomService = false,
+                HasSeaView = false,
+                HasTv = false,
+                HasWifi = false,
+                NumberOfBeds = 1,
+            }).GetAwaiter().GetResult();
+
+            roomRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            this.roomService = new RoomsService(roomRepository, roomReservationRepository, pictureService, moqCloudinaryService.Object);
+
+            AutoMapperConfig.RegisterMappings(typeof(MyTestRoom).Assembly);
+
+            var actualResult = await this.roomService
+                .GetAllRoomsAsync<MyTestRoom>();
+
+            int expectedResult = 1;
+            Assert.Equal(actualResult.Count(), expectedResult);
+        }
+
+        [Fact]
+        public async Task Get_All_Rooms_Should_Return_Zero()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                          .Options;
+
+            this.dbContext = new ApplicationDbContext(options);
+
+            var roomRepository = new EfDeletableEntityRepository<Room>(this.dbContext);
+            var roomReservationRepository = new EfDeletableEntityRepository<RoomReservation>(this.dbContext);
+            var pictureRepository = new EfDeletableEntityRepository<Picture>(this.dbContext);
+            var pictureService = new PictureService(pictureRepository);
+            var moqCloudinaryService = new Mock<ICloudinaryService>();
+
+
+            this.roomService = new RoomsService(roomRepository, roomReservationRepository, pictureService, moqCloudinaryService.Object);
+
+            AutoMapperConfig.RegisterMappings(typeof(MyTestRoom).Assembly);
+
+            var actualResult = await this.roomService
+                .GetAllRoomsAsync<MyTestRoom>();
+
+            int expectedResult = 0;
+            Assert.Equal(actualResult.Count(), expectedResult);
+        }
+
+        [Fact]
+        public async Task Get_Room_By_Id_Should_Return_Correct_Room()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                          .Options;
+
+            this.dbContext = new ApplicationDbContext(options);
+
+            var roomRepository = new EfDeletableEntityRepository<Room>(this.dbContext);
+            var roomReservationRepository = new EfDeletableEntityRepository<RoomReservation>(this.dbContext);
+            var pictureRepository = new EfDeletableEntityRepository<Picture>(this.dbContext);
+            var pictureService = new PictureService(pictureRepository);
+            var moqCloudinaryService = new Mock<ICloudinaryService>();
+
+            roomRepository.AddAsync(new Room
+            {
+                Id = "1",
+                RoomType = (RoomType)Enum.Parse(typeof(RoomType), "SingleRoom"),
+                Price = 10,
+                Pictures = null,
+                HasAirConditioner = true,
+                HasBathroom = true,
+                HasHeater = false,
+                HasMountainView = false,
+                HasPhone = false,
+                HasRoomService = false,
+                HasSeaView = false,
+                HasTv = false,
+                HasWifi = false,
+                NumberOfBeds = 1,
+            }).GetAwaiter().GetResult();
+
+            roomRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            this.roomService = new RoomsService(roomRepository, roomReservationRepository, pictureService, moqCloudinaryService.Object);
+
+            AutoMapperConfig.RegisterMappings(typeof(MyTestRoom).Assembly);
+
+            var actualResult = await this.roomService
+                .GetRoomAsync<MyTestRoom>("1");
+
+            string expectedResult = "1";
+            Assert.Equal(actualResult.Id, expectedResult);
+        }
+
+        [Fact]
+        public async Task Reserve_Room_Should_Make_One_Reservation()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                          .Options;
+
+            this.dbContext = new ApplicationDbContext(options);
+
+            var roomRepository = new EfDeletableEntityRepository<Room>(this.dbContext);
+            var roomReservationRepository = new EfDeletableEntityRepository<RoomReservation>(this.dbContext);
+            var pictureRepository = new EfDeletableEntityRepository<Picture>(this.dbContext);
+            var pictureService = new PictureService(pictureRepository);
+            var moqCloudinaryService = new Mock<ICloudinaryService>();
+
+            roomRepository.AddAsync(new Room
+            {
+                Id = "1",
+                RoomType = (RoomType)Enum.Parse(typeof(RoomType), "SingleRoom"),
+                Price = 10,
+                Pictures = null,
+                HasAirConditioner = true,
+                HasBathroom = true,
+                HasHeater = false,
+                HasMountainView = false,
+                HasPhone = false,
+                HasRoomService = false,
+                HasSeaView = false,
+                HasTv = false,
+                HasWifi = false,
+                NumberOfBeds = 1,
+            }).GetAwaiter().GetResult();
+
+            roomRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            this.roomService = new RoomsService(roomRepository, roomReservationRepository, pictureService, moqCloudinaryService.Object);
+
+            AutoMapperConfig.RegisterMappings(typeof(MyTestRoom).Assembly);
+
+            var reservation = new ReserveRoomInputModel
+            {
+                CheckIn = DateTime.Now.AddDays(1),
+                CheckOut = DateTime.Now.AddDays(3),
+                CountOfPeople = 2,
+                RoomId = "1",
+                Email = "maleksiev12@gmail.com",
+                FirstName = "Marian",
+                LastName = "Kyuchukov",
+                PhoneNumber = "0888186978",
+            };
+
+            var actualResult = await this.roomService.ReserveRoom(reservation);
+
+            Assert.True(actualResult);
+        }
+
+        [Fact]
+        public async Task Get_All_Reservations_Should_Return_All_Reservations()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                          .Options;
+
+            this.dbContext = new ApplicationDbContext(options);
+
+            var roomRepository = new EfDeletableEntityRepository<Room>(this.dbContext);
+            var roomReservationRepository = new EfDeletableEntityRepository<RoomReservation>(this.dbContext);
+            var pictureRepository = new EfDeletableEntityRepository<Picture>(this.dbContext);
+            var pictureService = new PictureService(pictureRepository);
+            var moqCloudinaryService = new Mock<ICloudinaryService>();
+
+            roomRepository.AddAsync(new Room
+            {
+                Id = "1",
+                RoomType = (RoomType)Enum.Parse(typeof(RoomType), "SingleRoom"),
+                Price = 10,
+                Pictures = null,
+                HasAirConditioner = true,
+                HasBathroom = true,
+                HasHeater = false,
+                HasMountainView = false,
+                HasPhone = false,
+                HasRoomService = false,
+                HasSeaView = false,
+                HasTv = false,
+                HasWifi = false,
+                NumberOfBeds = 1,
+            }).GetAwaiter().GetResult();
+
+            roomRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            roomReservationRepository.AddAsync(new RoomReservation
+            {
+                Id = "1",
+                RoomType = (RoomType)Enum.Parse(typeof(RoomType), "SingleRoom"),
+                CheckIn = DateTime.Now.AddDays(1),
+                CheckOut = DateTime.Now.AddDays(3),
+                NumberOfGuests = 2,
+                RoomId = "1",
+                UserId = "1",
+
+            }).GetAwaiter().GetResult();
+
+            roomReservationRepository.SaveChangesAsync().GetAwaiter().GetResult();
+
+            this.roomService = new RoomsService(roomRepository, roomReservationRepository, pictureService, moqCloudinaryService.Object);
+
+            AutoMapperConfig.RegisterMappings(typeof(MyTestRoom).Assembly);
+            AutoMapperConfig.RegisterMappings(typeof(MyTestReservation).Assembly);
+
+
+            int expectedResult = 1;
+
+            var actualResult = await this.roomService.GetAllReservationsAsync<MyTestReservation>("1");
+
+            Assert.Equal(actualResult.Count(), expectedResult);
+        }
+
     }
 }
